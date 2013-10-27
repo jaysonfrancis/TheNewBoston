b956e256c438f37442a0ec517c30cdd89193b39b
Jayson Francis <jaysonfrancis@gmail.com>
2013-10-27 00:28:28 -0700

    firstCommit

---
+++ src/main/java/com/jaysonfrancis/thenewboston/Splash.java
@@ -0,0 +1,34 @@
+package com.jaysonfrancis.thenewboston;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(5000);
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent openStartingPoint = new Intent("com.thenewboston.jaysonfrancis.MENU");
+                    startActivity(openStartingPoint);
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
---
+++ src/main/java/com/jaysonfrancis/thenewboston/startingPoint.java
@@ -0,0 +1,88 @@
+package com.jaysonfrancis.thenewboston;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class startingPoint extends ActionBarActivity {
+
+    int counter;
+    Button add, subtract;
+    TextView display;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        counter = 0;
+        add = (Button) findViewById(R.id.btnAdd);
+        subtract = (Button) findViewById(R.id.btnSub);
+        display = (TextView) findViewById(R.id.txtTotal);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                display.setText("Your total is " +counter);
+            }
+        });
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter--;
+                display.setText("Your total is " +counter);
+            }
+        });
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.starting_point, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
+}
